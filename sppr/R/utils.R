a81294c012b90b28a4062e28cc76dd27af21f67b
walter-weinmann <USR8fvB2fEZ4>
2020-12-13 06:27:42 +0100

    Base version.

---
+++ sppr/R/utils.R
@@ -0,0 +1,78 @@
+# Title : sppr_utils Objective : Utility functionality.
+# Created by: Walter Weinmann Created on: 16.11.2020
+
+
+#' Install the required packages via pacman.
+#'
+#' @import pacman
+#' @import utils
+#' @export
+
+sppr_install <- function() {
+
+  print("1. Step: Install pacman <============================================")
+
+  if (!"pacman" %in% installed.packages()) {
+    install.packages("pacman", repos = "https://cran.r-project.org/")
+  }
+
+  print("2. Step: Setting CRAN Repoitory <====================================")
+
+  pacman:::p_set_cranrepo(default_repo = "http://cran.r-project.org/")
+
+  print("3. Step: Load and install or update packages <=======================")
+
+  pacman::p_load("assertive",
+                 "data.table",
+                 "DBI",
+                 "devtools",
+                 "dplyr",
+                 "forecast",
+                 "formatR",
+                 "ggplot2",
+                 "installr",
+                 "knitr",
+                 "lintr",
+                 "magrittr",
+                 "pillar",
+                 "R6",
+                 "readr",
+                 "rmarkdown",
+                 "roxygen2",
+                 "RSQLite",
+                 "stringr",
+                 "testthat",
+                 "TSstudio",
+                 "xts",
+                 "zoo",
+                 install = TRUE,
+                 update = TRUE
+  )
+
+  print("4. Step: Verify packages <===========================================")
+
+  pacman::p_loaded("assertive",
+                   "data.table",
+                   "DBI",
+                   "devtools",
+                   "dplyr",
+                   "forecast",
+                   "formatR",
+                   "ggplot2",
+                   "installr",
+                   "knitr",
+                   "lintr",
+                   "magrittr",
+                   "pillar",
+                   "R6",
+                   "readr",
+                   "rmarkdown",
+                   "roxygen2",
+                   "RSQLite",
+                   "stringr",
+                   "testthat",
+                   "TSstudio",
+                   "xts",
+                   "zoo"
+  )
+}
