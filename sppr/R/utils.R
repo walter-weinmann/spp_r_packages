ad898ff76759e07ff41c578136302703482f456a
walter-weinmann <USR8fvB2fEZ4>
2020-12-14 08:59:01 +0100

    Base version.

--- sppr/R/utils.R
+++ sppr/R/utils.R
@@ -3,7 +3,7 @@
 # +nn Created on: 16.11.2020
 
 # ==============================================================================
-#' Initialize packrat.
+#' Install and initialize the package packrat.
 #'
 #' @import packrat
 #' @import utils
@@ -16,6 +16,7 @@ init_packrat <- function() {
   if (!"packrat" %in% installed.packages()) {
     install.packages("packrat",
                      repos = "https://cran.r-project.org/")
+    print("Installed package: packrat")
   }
 
   print("2. Step: Initialize packrat <========================================")
@@ -28,22 +29,45 @@ init_packrat <- function() {
 }
 
 # ==============================================================================
-#' Install the required packages.
+#' Install the required packages and show the current installation state.
 #'
 #' @import utils
 #' @export
 # ------------------------------------------------------------------------------
 
-install <- function() {
+install_required <- function() {
   print("1. Step: Install the required packages <=============================")
 
   for (package in required_packages()) {
     if (!package %in% installed.packages()) {
       install.packages(package, repos = "https://cran.r-project.org/")
+      print("Installed package:", package)
     }
   }
 
-  print("2. Step: Show the installation result <==============================")
+  print("2. Step: Show the current installation state <=======================")
+
+  show_installed()
+}
+
+# ==============================================================================
+#' Remove required packages and show the current installation state.
+#'
+#' @import utils
+#' @export
+# ------------------------------------------------------------------------------
+
+remove_required <- function() {
+  print("1. Step: Remove the required packages <==============================")
+
+  for (package in required_packages()) {
+    if (package %in% installed.packages()) {
+      remove.packages(package)
+      print("Removed package:", package)
+    }
+  }
+
+  print("2. Step: Show the current installation state <=======================")
 
   show_installed()
 }
@@ -82,7 +106,7 @@ required_packages <- function(add_ons = vector()) {
 }
 
 # ==============================================================================
-#' Show tht installed packages.
+#' Show the status of the required packages.
 #'
 #' @export
 # ------------------------------------------------------------------------------
