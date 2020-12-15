a4f82fb5dcac88602b00a251c94cee7f5c806d68
walter-weinmann <USR8fvB2fEZ4>
2020-12-15 12:02:55 +0100

    Script utils.

--- sppr/R/utils.R
+++ sppr/R/utils.R
@@ -1,6 +1,39 @@
-# Title : sppr_utils Objective : Utility functionality.
+# Title : package_management Objective : Package Management.
 # Created by: Walter Weinma/
 # +nn Created on: 16.11.2020
+
+cran_repository <- "https://cran.r-project.org/"
+
+# Definition of the basic packages outside sppr.
+packages_basic <- c("devtools",
+                    "sppr")
+
+# Definition of the packages imported by sppr.
+packages_imported <- c("dplyr",
+                       "magrittr",
+                       "roxygen2",
+                       "RSQLite",
+                       "stringr",
+                       "utils")
+
+# Definition of the other sppr packages.
+packages_others <- c("assertive",
+                     "data.table",
+                     "DBI",
+                     "forecast",
+                     "formatR",
+                     "ggplot2",
+                     "installr",
+                     "knitr",
+                     "lintr",
+                     "pillar",
+                     "R6",
+                     "readr",
+                     "rmarkdown",
+                     "testthat",
+                     "TSstudio",
+                     "xts",
+                     "zoo")
 
 # ==============================================================================
 #' Create a combined and sorted character vector containing the necessary
@@ -16,7 +49,7 @@ get_packages <- function(add_on_1 = vector(), add_on_2 = vector()) {
                     after = length(add_on_1))
 
   sort(append(add_ons,
-              packages_others(),
+              packages_others,
               after = length(add_ons)))
 }
 
@@ -33,11 +66,11 @@ get_packages <- function(add_on_1 = vector(), add_on_2 = vector()) {
 install <- function(library = .libPaths()[1]) {
   print("1. Step: Install the required packages <=============================")
 
-  for (package in get_packages(packages_imported())) {
+  for (package in get_packages(packages_imported)) {
     if (length(find.package(package, lib.loc = library, quiet = TRUE)) == 0) {
       install.packages(package,
                        library,
-                       repos = "https://cran.r-project.org/",
+                       repos = cran_repository,
                        force = TRUE)
       print(paste("Installed package:", package, sep = " "))
     }
@@ -45,53 +78,7 @@ install <- function(library = .libPaths()[1]) {
 
   print("2. Step: Show the current installation state <=======================")
 
-  show_specific(library)
-}
-
-# ==============================================================================
-#' Definition of the sppr packages: basic packages.
-# ------------------------------------------------------------------------------
-
-packages_basic <- function() {
-  c("devtools",
-    "sppr")
-}
-
-# ==============================================================================
-#' Definition of the sppr packages: imported packages.
-# ------------------------------------------------------------------------------
-
-packages_imported <- function() {
-  c("dplyr",
-    "magrittr",
-    "roxygen2",
-    "RSQLite",
-    "stringr",
-    "utils")
-}
-
-# ==============================================================================
-#' Definition of the sppr packages: other packages.
-# ------------------------------------------------------------------------------
-
-packages_others <- function() {
-  c("assertive",
-    "data.table",
-    "DBI",
-    "forecast",
-    "formatR",
-    "ggplot2",
-    "installr",
-    "knitr",
-    "lintr",
-    "pillar",
-    "R6",
-    "readr",
-    "rmarkdown",
-    "testthat",
-    "TSstudio",
-    "xts",
-    "zoo")
+  show_packages(library)
 }
 
 # ==============================================================================
@@ -115,7 +102,7 @@ remove <- function(library = .libPaths()[1]) {
 
   print("2. Step: Show the current installation state <=======================")
 
-  show_specific(library)
+  show_packages(library)
 }
 
 # ==============================================================================
@@ -126,11 +113,32 @@ remove <- function(library = .libPaths()[1]) {
 #' @export
 # ------------------------------------------------------------------------------
 
-show_specific <- function(library = .libPaths()[1]) {
-  sapply(get_packages(packages_basic(),
-                      packages_imported()),
+show_packages <- function(library = .libPaths()[1]) {
+  sapply(get_packages(packages_basic,
+                      packages_imported),
          function(x) {
            x %in% installed.packages(library)
          }
   )
+}
+
+# ==============================================================================
+#' Update all packages and show the resulting installation state.
+#'
+#' @param library Character vector describing the location of R library
+#'                trees to search through
+#' @import utils
+#' @export
+# ------------------------------------------------------------------------------
+
+update <- function(library = .libPaths()[1]) {
+  print("1. Step: Updata all packages <=======================================")
+
+  update.packages(lib.loc = library,
+                  repos   = cran_repository,
+                  ask     = TRUE)
+
+  print("2. Step: Show the current installation state <=======================")
+
+  show_packages(library)
 }
